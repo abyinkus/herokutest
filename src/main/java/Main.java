4f82c17c45e0ff5c115a1ed4a048e54a4822ff27
ape10 <kmahmassani@gmail.com>
2017-07-13 14:25:23 +0100

    ttt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,6 +28,11 @@ public class Main {
               ctx.render("Hello!");
             })
 
+            .get("testing", ctx -> {
+              ctx.render("look at this!");
+            })
+
+
             .get("db", ctx -> {
               boolean local = !"cedar-14".equals(System.getenv("STACK"));
 
